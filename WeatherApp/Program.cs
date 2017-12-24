5a1dd98595f85c8d714e3fdc956f4c6ff3c0094c
KaroshiInProgress <nooodels@roiltd.co.uk>
2017-12-24 16:13:14 +0000

    Stage 3

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Dynamic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WeatherApp
 {
     class Program
     {
+        private static string[] ValidSections = { "weather", "wind", "main", "clouds" };
+
         static void Main(string[] args)
         {
-            if (args.Length != 3)
+            if (args.Length != 3 || args.Length != 4)
             {
                 Console.WriteLine("You are missing some arguments.");
                 Environment.Exit(0);
@@ -19,11 +24,26 @@ namespace WeatherApp
             var apiKey = args[0];
             var cityName = args[1];
             var countryCode = args[2];
+            string section = null;
+
+            if (args.Length == 4)
+            {
+                if (ValidSections.Contains(args[3].ToLower()))
+                {
+                    section = args[3].ToLower();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid section.");
+                    Environment.Exit(0);
+                }
+            }
 
             // for dev
             //var apiKey = "910212c8b2a6d73f2988da96ed5a56d0";
             //var cityName = "Nantwich";
             //var countryCode = "UK";
+            //string section = null;
 
             var httpResponseTask = GetWeatherData(apiKey, cityName, countryCode);
             httpResponseTask.Wait();
@@ -37,10 +57,39 @@ namespace WeatherApp
             var weatherDataTask = httpResponseTask.Result.Content.ReadAsStringAsync();
             weatherDataTask.Wait();
 
+            var result = weatherDataTask.Result;
+            if (section != null)
+            {
+                dynamic resultObject = JsonConvert.DeserializeObject<ExpandoObject>(weatherDataTask.Result);
+
+                switch (section)
+                {
+                    case "weather":
+                        result = JsonConvert.SerializeObject(resultObject.weather);
+                        break;
+
+                    case "wind":
+                        result = JsonConvert.SerializeObject(resultObject.wind);
+                        break;
+
+                    case "main":
+                        result = JsonConvert.SerializeObject(resultObject.main);
+                        break;
+
+                    case "clouds":
+                        result = JsonConvert.SerializeObject(resultObject.clouds);
+                        break;
+                }
+            }
+
             Console.WriteLine($"ApiKey: {apiKey}");
             Console.WriteLine($"CityName: {cityName}");
             Console.WriteLine($"CountryCode: {countryCode}");
-            Console.WriteLine($"Results: {weatherDataTask.Result}");
+            if (section != null)
+            {
+                Console.WriteLine($"Section: {section}");
+            }
+            Console.WriteLine($"Results: {result}");
             Console.ReadKey();
         }
 
