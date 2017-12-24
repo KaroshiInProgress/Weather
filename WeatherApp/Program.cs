dbc38b0a396e712bd20d98cff08719ea9cad79ac
Alex Weston <alexander.weston@roiltd.co.uk>
2017-12-24 15:12:31 +0000

    stage 1

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WeatherApp
 {
@@ -10,6 +6,20 @@ namespace WeatherApp
     {
         static void Main(string[] args)
         {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("You are missing some arguments.");
+                Environment.Exit(0);
+            }
+
+            var apiKey = args[0];
+            var cityName = args[1];
+            var countryCode = args[2];
+
+            Console.WriteLine($"ApiKey: {apiKey}");
+            Console.WriteLine($"CityName: {cityName}");
+            Console.WriteLine($"CountryCode: {countryCode}");
+            Console.ReadKey();
         }
     }
 }
