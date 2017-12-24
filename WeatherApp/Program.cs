90d7251184fdc78a759d2555566c51b589e336a1
KaroshiInProgress <nooodels@roiltd.co.uk>
2017-12-24 15:49:11 +0000

    stage 2

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace WeatherApp
 {
@@ -16,10 +20,41 @@ namespace WeatherApp
             var cityName = args[1];
             var countryCode = args[2];
 
+            // for dev
+            //var apiKey = "910212c8b2a6d73f2988da96ed5a56d0";
+            //var cityName = "Nantwich";
+            //var countryCode = "UK";
+
+            var httpResponseTask = GetWeatherData(apiKey, cityName, countryCode);
+            httpResponseTask.Wait();
+
+            if (httpResponseTask.Result.StatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine("Unable to contact weather API.");
+                Environment.Exit(0);
+            }
+
+            var weatherDataTask = httpResponseTask.Result.Content.ReadAsStringAsync();
+            weatherDataTask.Wait();
+
             Console.WriteLine($"ApiKey: {apiKey}");
             Console.WriteLine($"CityName: {cityName}");
             Console.WriteLine($"CountryCode: {countryCode}");
+            Console.WriteLine($"Results: {weatherDataTask.Result}");
             Console.ReadKey();
+        }
+
+        private static async Task<HttpResponseMessage> GetWeatherData(string apiKey, string cityName, string countryCode)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://api.openweathermap.org/data/2.5/weather");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var queryString = $"?q={cityName},{countryCode}&APPID={apiKey}";
+                return await client.GetAsync(queryString);
+            }
         }
     }
 }
